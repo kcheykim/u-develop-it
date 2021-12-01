37d0eaef0a7b8cc07c70ca3a3fce9289c42c0fea
kcheykim <kosalcheykim@gmail.com>
2021-12-01 13:45:39 -0800

    getting the 2 get for the voterRoutes

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -19,3 +19,20 @@ router.get('/voters', (req, res) => {
 });
 
 module.exports = router;
+
+// Get single voter
+router.get('/voter/:id', (req, res) => {
+    const sql = `SELECT * FROM voters WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
